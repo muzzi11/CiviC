14aee85a2fdd86dd15f27614bd9a22c006514207
Muzzi11 <muzzi11@hotmail.com>
2014-04-10 00:46:49 +0200

    final

--- civicc/civicc/main.cpp
+++ civicc/civicc/main.cpp
@@ -25,6 +25,10 @@ int main(int argc, char* argv[])
 	for(int i = 1; i < argc; ++i)
 	{
 		if(strcmp(argv[i], "-v") == 0) verbose = true;
+		else if(strcmp(argv[i], "--help") == 0)
+		{
+			std::cout << "civicc [-v] [-o <file>] [<file>]\n";
+		}
 		else if(strcmp(argv[i], "-o") == 0)
 		{
 			if(i + 1 >= argc)
